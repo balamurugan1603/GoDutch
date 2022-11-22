dd3dcbd989e84ba23062bcec131615a0be51ff7e
Balamurugan P <75413685+balamurugan1603@users.noreply.github.com>
2022-11-22 10:11:40 +0530

    Added TODO for doctor

---
+++ GoDutch/doctor/doctor.py
@@ -0,0 +1,7 @@
+# TODO
+# Broadcast heartbeat to all devices
+# If response received back, device is alive
+# If no response
+#   - Either device is dead
+#   - Or work is complete
+#       - Check DB to verify
